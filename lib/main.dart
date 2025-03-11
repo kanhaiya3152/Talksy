9054e2d2977d8f61fe66b6f8ee81a22eaf3f8c98
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-03-11 21:56:25 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   // await Firebase.initializeApp();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
