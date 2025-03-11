bd4b0aeb2c2f231bc181aed15a79314a883c18dc
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-03-11 21:21:12 +0530

    added navigation buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/foundation.dart';
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_messages.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.uid});
+  final String uid;
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -13,11 +16,22 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Chat "),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => ChatScreen(),
+              ),
+            );
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+        title: Text("Chats"),
       ),
       body: Column(
         children: [
-          
+          Expanded(child: ChatMessages(receiverId: widget.uid)),
+          NewMessages(receiverId: widget.uid),
         ],
       ),
     );
